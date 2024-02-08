45d9dafdb969e0f7754607ee67c45dea14667150
Antonis Kalou <kalouantonis@protonmail.com>
2024-02-08 18:08:03 +0200

    Print datarefs in flight loop

--- plugins/logbook-xp12/src/lib.rs
+++ plugins/logbook-xp12/src/lib.rs
@@ -1,10 +1,10 @@
 use xplm::data::borrowed::{DataRef, FindError};
 use xplm::data::{ArrayRead, DataRead, ReadOnly, StringRead};
-use xplm::flight_loop::{FlightLoop, LoopState};
+use xplm::flight_loop::{FlightLoop, FlightLoopCallback, LoopState};
 use xplm::plugin::{Plugin, PluginInfo};
 use xplm::{debug, xplane_plugin};
 
-struct LogbookPlugin {
+struct FlightLoopHandler {
     latitude: DataRef<f64, ReadOnly>,
     longitude: DataRef<f64, ReadOnly>,
     icao: DataRef<[u8], ReadOnly>,
@@ -12,19 +12,11 @@ struct LogbookPlugin {
     registration: DataRef<[u8], ReadOnly>,
     engine_on: DataRef<[i32], ReadOnly>,
     on_ground: DataRef<bool, ReadOnly>,
-    flight_loop: FlightLoop,
 }
 
-impl Plugin for LogbookPlugin {
-    type Error = FindError;
-
-    fn start() -> Result<Self, Self::Error> {
-        debug("Hello from logbook\n");
-        let handler = |_: &mut LoopState| {
-            debug("Flight loop callback running\n");
-        };
-        let flight_loop = FlightLoop::new(handler);
-        Ok(LogbookPlugin {
+impl FlightLoopHandler {
+    fn new() -> Result<Self, FindError> {
+        Ok(Self {
             latitude: DataRef::find("sim/flightmodel/position/latitude")?,
             longitude: DataRef::find("sim/flightmodel/position/longitude")?,
             icao: DataRef::find("sim/aircraft/view/acf_ICAO")?,
@@ -33,13 +25,12 @@ impl Plugin for LogbookPlugin {
             engine_on: DataRef::find("sim/flightmodel/engine/ENGN_running")?,
             // according to the docs: "User Aircraft is on the ground when this is set to 1"
             on_ground: DataRef::find("sim/flightmodel/failures/onground_any")?,
-            flight_loop,
         })
     }
+}
 
-    fn enable(&mut self) -> Result<(), Self::Error> {
-        self.flight_loop.schedule_after(std::time::Duration::from_secs(1));
-
+impl FlightLoopCallback for FlightLoopHandler {
+    fn flight_loop(&mut self, _: &mut LoopState) {
         debug(format!("Latitude: {}\n", self.latitude.get()));
         debug(format!("Longitude: {}\n", self.longitude.get()));
 
@@ -57,7 +48,24 @@ impl Plugin for LogbookPlugin {
         debug(format!("Registration: {}\n", reg));
         debug(format!("Engine On? {:?}\n", self.engine_on.as_vec()));
         debug(format!("On ground? {}\n", self.on_ground.get()));
+    }
+}
 
+struct LogbookPlugin {
+    flight_loop: FlightLoop,
+}
+
+impl Plugin for LogbookPlugin {
+    type Error = FindError;
+
+    fn start() -> Result<Self, Self::Error> {
+        debug("Hello from logbook\n");
+        let flight_loop = FlightLoop::new(FlightLoopHandler::new()?);
+        Ok(LogbookPlugin { flight_loop, })
+    }
+
+    fn enable(&mut self) -> Result<(), Self::Error> {
+        self.flight_loop.schedule_after(std::time::Duration::from_secs(1));
         Ok(())
     }
 
