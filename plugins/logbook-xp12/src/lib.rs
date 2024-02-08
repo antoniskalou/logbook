a2d5afe03f609bd719a2ce4aae4ece34f8f6274e
Antonis Kalou <kalouantonis@protonmail.com>
2024-02-08 22:14:34 +0200

    Dont send data when replay is active

--- plugins/logbook-xp12/src/lib.rs
+++ plugins/logbook-xp12/src/lib.rs
@@ -7,6 +7,10 @@ use xplm::plugin::{Plugin, PluginInfo};
 use xplm::{debug, xplane_plugin};
 
 struct FlightLoopHandler {
+    tcp_listener: std::net::TcpListener,
+    tcp_connections: Vec<std::net::TcpStream>,
+    is_in_replay: DataRef<bool, ReadOnly>,
+    // datarefs for transfer
     latitude: DataRef<f64, ReadOnly>,
     longitude: DataRef<f64, ReadOnly>,
     icao: DataRef<[u8], ReadOnly>,
@@ -14,8 +18,6 @@ struct FlightLoopHandler {
     registration: DataRef<[u8], ReadOnly>,
     engine_on: DataRef<[i32], ReadOnly>,
     on_ground: DataRef<bool, ReadOnly>,
-    tcp_listener: std::net::TcpListener,
-    tcp_connections: Vec<std::net::TcpStream>,
 }
 
 impl FlightLoopHandler {
@@ -24,6 +26,9 @@ impl FlightLoopHandler {
         tcp_listener.set_nonblocking(true).unwrap();
 
         Ok(Self {
+            tcp_listener,
+            tcp_connections: vec![],
+            is_in_replay: DataRef::find("sim/time/is_in_replay")?,
             latitude: DataRef::find("sim/flightmodel/position/latitude")?,
             longitude: DataRef::find("sim/flightmodel/position/longitude")?,
             icao: DataRef::find("sim/aircraft/view/acf_ICAO")?,
@@ -32,31 +37,45 @@ impl FlightLoopHandler {
             engine_on: DataRef::find("sim/flightmodel/engine/ENGN_running")?,
             // according to the docs: "User Aircraft is on the ground when this is set to 1"
             on_ground: DataRef::find("sim/flightmodel/failures/onground_any")?,
-            tcp_listener,
-            tcp_connections: vec![],
         })
     }
 }
 
 impl FlightLoopCallback for FlightLoopHandler {
     fn flight_loop(&mut self, _: &mut LoopState) {
-        // debug(format!("Latitude: {}\n", self.latitude.get()));
-        // debug(format!("Longitude: {}\n", self.longitude.get()));
+        if self.is_in_replay.get() {
+            // dont do anything if we're replaying, it will break the logging
+            debug("Entered replay mode, pausing transmissions.\n");
+            return;
+        }
 
-        // let icao = self.icao
-        //     .get_as_string()
-        //     .unwrap_or(String::from("Unknown"));
-        // let name = self.name
-        //     .get_as_string()
-        //     .unwrap_or(String::from("Unknown"));
-        // let reg = self.registration
-        //     .get_as_string()
-        //     .unwrap_or(String::from("Unknown"));
-        // debug(format!("ICAO: {}\n", icao));
-        // debug(format!("Name: {}\n", name));
-        // debug(format!("Registration: {}\n", reg));
-        // debug(format!("Engine On? {:?}\n", self.engine_on.as_vec()));
-        // debug(format!("On ground? {}\n", self.on_ground.get()));
+        let lat = self.latitude.get();
+        let lon = self.longitude.get();
+        let icao = self.icao
+            .get_as_string()
+            .unwrap_or(String::from("Unknown"));
+        let name = self.name
+            .get_as_string()
+            .unwrap_or(String::from("Unknown"));
+        let reg = self.registration
+            .get_as_string()
+            .unwrap_or(String::from("Unknown"));
+        let engine_on = self.engine_on
+            .as_vec()
+            .iter()
+            .any(|x| *x == 1);
+        let on_ground = self.on_ground.get();
+
+        let record = [
+            format!("lat={}", lat.to_string()),
+            format!("lon={}", lon.to_string()),
+            format!("icao={}", icao),
+            format!("name={}", name),
+            format!("reg={}", reg),
+            format!("engine_on={}", engine_on.to_string()),
+            format!("on_ground={}", on_ground.to_string()),
+        ];
+        let record_str = format!("{}\r\n", record.join(","));
 
         match self.tcp_listener.accept() {
             Ok((socket, addr)) => {
@@ -69,7 +88,7 @@ impl FlightLoopCallback for FlightLoopHandler {
         }
 
         self.tcp_connections.retain_mut(|stream| {
-            match stream.write_all(b"hello") {
+            match stream.write_all(record_str.as_bytes()) {
                 Ok(_) => {
                     debug(format!("Wrote to stream: {stream:?}\n"));
                     true
