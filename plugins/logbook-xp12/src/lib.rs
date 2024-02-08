1df78f5f9a3454fdcee9bf9657a41868de839d96
Antonis Kalou <kalouantonis@protonmail.com>
2024-02-08 22:34:44 +0200

    Cleanup record creation

--- plugins/logbook-xp12/src/lib.rs
+++ plugins/logbook-xp12/src/lib.rs
@@ -1,5 +1,4 @@
-use std::io::Write;
-
+use std::{io::Write, net::TcpListener};
 use xplm::data::borrowed::{DataRef, FindError};
 use xplm::data::{ArrayRead, DataRead, ReadOnly, StringRead};
 use xplm::flight_loop::{FlightLoop, FlightLoopCallback, LoopState};
@@ -22,7 +21,8 @@ struct FlightLoopHandler {
 
 impl FlightLoopHandler {
     fn new() -> Result<Self, FindError> {
-        let tcp_listener = std::net::TcpListener::bind("127.0.0.1:52000").unwrap();
+        // TODO: handle errors
+        let tcp_listener = TcpListener::bind("127.0.0.1:52000").unwrap();
         tcp_listener.set_nonblocking(true).unwrap();
 
         Ok(Self {
@@ -39,6 +39,27 @@ impl FlightLoopHandler {
             on_ground: DataRef::find("sim/flightmodel/failures/onground_any")?,
         })
     }
+
+    fn as_record(&self) -> Vec<String> {
+        let icao = self.icao
+            .get_as_string()
+            .unwrap_or(String::from("UNKNOWN"));
+        let name = self.name
+            .get_as_string()
+            .unwrap_or(String::from("UNKNOWN"));
+        let reg = self.registration
+            .get_as_string()
+            .unwrap_or(String::from("UNKNOWN"));
+        let lat = self.latitude.get().to_string();
+        let lon = self.longitude.get().to_string();
+        let engine_on = self.engine_on
+            .as_vec()
+            .iter()
+            .any(|x| *x == 1)
+            .to_string();
+        let on_ground = self.on_ground.get().to_string();
+        vec![icao, name, reg, lat, lon, engine_on, on_ground]
+    }
 }
 
 impl FlightLoopCallback for FlightLoopHandler {
@@ -48,34 +69,6 @@ impl FlightLoopCallback for FlightLoopHandler {
             debug("Entered replay mode, pausing transmissions.\n");
             return;
         }
-
-        let lat = self.latitude.get();
-        let lon = self.longitude.get();
-        let icao = self.icao
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        let name = self.name
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        let reg = self.registration
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        let engine_on = self.engine_on
-            .as_vec()
-            .iter()
-            .any(|x| *x == 1);
-        let on_ground = self.on_ground.get();
-
-        let record = [
-            format!("lat={}", lat.to_string()),
-            format!("lon={}", lon.to_string()),
-            format!("icao={}", icao),
-            format!("name={}", name),
-            format!("reg={}", reg),
-            format!("engine_on={}", engine_on.to_string()),
-            format!("on_ground={}", on_ground.to_string()),
-        ];
-        let record_str = format!("{}\r\n", record.join(","));
 
         match self.tcp_listener.accept() {
             Ok((socket, addr)) => {
@@ -87,15 +80,16 @@ impl FlightLoopCallback for FlightLoopHandler {
             Err(e) => debug(format!("IO error: {e}\n")),
         }
 
+        let record_line = format!("{}\r\n", self.as_record().join(","));
         self.tcp_connections.retain_mut(|stream| {
-            match stream.write_all(record_str.as_bytes()) {
+            match stream.write_all(record_line.as_bytes()) {
                 Ok(_) => {
                     debug(format!("Wrote to stream: {stream:?}\n"));
                     true
                 },
                 // client closed connection
                 Err(ref e) if e.kind() == std::io::ErrorKind::ConnectionAborted => {
-                    debug(format!("Client closed connection...\n"));
+                    debug("Client closed connection...\n");
                     false
                 }
                 Err(e) => {
@@ -115,7 +109,7 @@ impl Plugin for LogbookPlugin {
     type Error = FindError;
 
     fn start() -> Result<Self, Self::Error> {
-        debug("Hello from logbook\n");
+        debug("Logbook plugin started.\n");
         let flight_loop = FlightLoop::new(FlightLoopHandler::new()?);
         Ok(LogbookPlugin { flight_loop, })
     }
@@ -139,8 +133,3 @@ impl Plugin for LogbookPlugin {
 }
 
 xplane_plugin!(LogbookPlugin);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-}
