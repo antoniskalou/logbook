9df5a5977b4108ee4dd5305d385b252e0fa19ce9
Antonis Kalou <kalouantonis@protonmail.com>
2024-02-08 20:23:03 +0200

    Add TCP server in xp plugin

--- plugins/logbook-xp12/src/lib.rs
+++ plugins/logbook-xp12/src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use xplm::data::borrowed::{DataRef, FindError};
 use xplm::data::{ArrayRead, DataRead, ReadOnly, StringRead};
 use xplm::flight_loop::{FlightLoop, FlightLoopCallback, LoopState};
@@ -12,10 +14,15 @@ struct FlightLoopHandler {
     registration: DataRef<[u8], ReadOnly>,
     engine_on: DataRef<[i32], ReadOnly>,
     on_ground: DataRef<bool, ReadOnly>,
+    tcp_listener: std::net::TcpListener,
+    tcp_connections: Vec<std::net::TcpStream>,
 }
 
 impl FlightLoopHandler {
     fn new() -> Result<Self, FindError> {
+        let tcp_listener = std::net::TcpListener::bind("127.0.0.1:52000").unwrap();
+        tcp_listener.set_nonblocking(true).unwrap();
+
         Ok(Self {
             latitude: DataRef::find("sim/flightmodel/position/latitude")?,
             longitude: DataRef::find("sim/flightmodel/position/longitude")?,
@@ -25,29 +32,59 @@ impl FlightLoopHandler {
             engine_on: DataRef::find("sim/flightmodel/engine/ENGN_running")?,
             // according to the docs: "User Aircraft is on the ground when this is set to 1"
             on_ground: DataRef::find("sim/flightmodel/failures/onground_any")?,
+            tcp_listener,
+            tcp_connections: vec![],
         })
     }
 }
 
 impl FlightLoopCallback for FlightLoopHandler {
     fn flight_loop(&mut self, _: &mut LoopState) {
-        debug(format!("Latitude: {}\n", self.latitude.get()));
-        debug(format!("Longitude: {}\n", self.longitude.get()));
+        // debug(format!("Latitude: {}\n", self.latitude.get()));
+        // debug(format!("Longitude: {}\n", self.longitude.get()));
 
-        let icao = self.icao
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        let name = self.name
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        let reg = self.registration
-            .get_as_string()
-            .unwrap_or(String::from("Unknown"));
-        debug(format!("ICAO: {}\n", icao));
-        debug(format!("Name: {}\n", name));
-        debug(format!("Registration: {}\n", reg));
-        debug(format!("Engine On? {:?}\n", self.engine_on.as_vec()));
-        debug(format!("On ground? {}\n", self.on_ground.get()));
+        // let icao = self.icao
+        //     .get_as_string()
+        //     .unwrap_or(String::from("Unknown"));
+        // let name = self.name
+        //     .get_as_string()
+        //     .unwrap_or(String::from("Unknown"));
+        // let reg = self.registration
+        //     .get_as_string()
+        //     .unwrap_or(String::from("Unknown"));
+        // debug(format!("ICAO: {}\n", icao));
+        // debug(format!("Name: {}\n", name));
+        // debug(format!("Registration: {}\n", reg));
+        // debug(format!("Engine On? {:?}\n", self.engine_on.as_vec()));
+        // debug(format!("On ground? {}\n", self.on_ground.get()));
+
+        match self.tcp_listener.accept() {
+            Ok((socket, addr)) => {
+                debug(format!("new client: {addr:?}\n"));
+                socket.set_nonblocking(true).unwrap();
+                self.tcp_connections.push(socket);
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {},
+            Err(e) => debug(format!("IO error: {e}\n")),
+        }
+
+        self.tcp_connections.retain_mut(|stream| {
+            match stream.write_all(b"hello") {
+                Ok(_) => {
+                    debug(format!("Wrote to stream: {stream:?}\n"));
+                    true
+                },
+                // client closed connection
+                Err(ref e) if e.kind() == std::io::ErrorKind::ConnectionAborted => {
+                    debug(format!("Client closed connection...\n"));
+                    false
+                }
+                Err(e) => {
+                    debug(format!("TCP client error: {e}"));
+                    false
+                }
+            }
+        });
     }
 }
 
